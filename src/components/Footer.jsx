cb39d958eab932769433cfeaee017b66ad71c1b8
Akshat <akshatiscoding@gmail.com>
2024-10-16 20:00:43 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,43 +17,9 @@ const Footer = () => {
   return (
     <footer className="relative bg-black gap-52 md:gap-0">
       <div className="flex flex-col items-center gap-20 px-8 py-4 justify-evenly md:justify-between md:flex-row ">
-        <div className="footerNav ">
-          <ul className="flex flex-wrap gap-4 justify-evenly md:justify-between">
-            {[
-              { label: "News", icon: FaNewspaper, content: "News Content" },
-              {
-                label: "About Us",
-                icon: AiOutlineInfoCircle,
-                content: "About Us Content",
-              },
-              {
-                label: "Contact Us",
-                icon: AiOutlineMessage,
-                content: "Contact Us Content",
-                link:'/contact'
-              },
-              {
-                label: "Our Team",
-                icon: AiOutlineTeam,
-                content: "Our Team Content",
-                link:'/team'
-              },
-            ].map((item, index) => (
-              <li key={index}>
-                <a
-                  href={item.link}
-                  className="flex items-center text-white hover:text-gray-600"
-                >
-                  <item.icon className="w-6 h-6" />
-                  <span className="ml-2">{item.label}</span>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
         <div className="flex flex-wrap gap-4 socialicons icons">
           <a
-            href="https://www.linkedin.com/in/singodiyashubham87/"
+            href="https://www.linkedin.com/in/akshatnathani/"
             className="group"
           >
             <div className="transform icon group-hover:scale-125">
@@ -63,7 +29,7 @@ const Footer = () => {
               />
             </div>
           </a>
-          <a href="https://twitter.com/_master_mickey" className="group">
+          <a href="https://twitter.com/kyzolia" className="group">
             <div className="transform icon group-hover:scale-125">
               <FaTwitter
                 className="w-8 h-8 text-white group-hover:text-twitter"
@@ -71,7 +37,7 @@ const Footer = () => {
               />
             </div>
           </a>
-          <a href="https://www.instagram.com" className="group">
+          <a href="https://www.instagram.com/akkshth" className="group">
             <div className="relative transform icon group-hover:scale-125">
               <FaInstagram
                 className="relative z-10 w-8 h-8 text-white group-hover:text-instagram"
@@ -79,15 +45,7 @@ const Footer = () => {
               />
             </div>
           </a>
-          <a href="https://www.youtube.com" className="group">
-            <div className="transform icon group-hover:scale-125">
-              <FaYoutube
-                className="w-8 h-8 text-white group-hover:text-youtube"
-                style={{ transition: "color 0.3s" }}
-              />
-            </div>
-          </a>
-          <a href="https://github.com/singodiyashubham87" className="group">
+          <a href="https://github.com/akshat-nathani" className="group">
             <div className="transform icon group-hover:scale-125">
               <FaGithub
                 className="w-8 h-8 text-white group-hover:text-github"
@@ -101,10 +59,10 @@ const Footer = () => {
         <p className="text-white">
           Made with 💝 by{" "}
           <a
-            href="https://shubham-s-socials.vercel.app/"
+            href="https://linkedin.com/in/akshatnathani"
             className="font-semibold decoration-none hover:underline"
           >
-            Master Mickey
+            Akshat Nathani
           </a>{" "}
           Copyright &copy;{new Date().getFullYear()}
         </p>
